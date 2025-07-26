91a09892dd5ee4f9639a9fdbc2ceb4d6b4e15c5e
izasoerya <ihza.surya@gmail.com>
2025-07-26 14:14:13 +0700

    feat: implement modal bottom sheet

--- app/src/main/java/com/example/asetmu/screens/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/asetmu/screens/dashboard/DashboardScreen.kt
@@ -1,21 +1,48 @@
 package com.example.asetmu.screens.dashboard
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.Colors
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen(
 	navController: NavController
 ) {
-	Column(
+	val configuration = LocalConfiguration.current
+	val screenWidth = configuration.screenWidthDp.dp
+	val iconSize = remember { screenWidth * 0.05f }
+	val sheetState = rememberModalBottomSheetState ()
+	val showBottomSheet = remember { mutableStateOf(false) }
+
+	Box(
 		modifier = Modifier.fillMaxWidth()
 	){
 		GoogleMap (
@@ -23,8 +50,28 @@ fun DashboardScreen(
 				isMyLocationEnabled = true,
 				isBuildingEnabled = true,
 			)
-		) {  }
-		Button(onClick = { navController.navigate("login_screen") }) {}
-		Text("Dashboard Screen", modifier = Modifier.fillMaxWidth())
+		)
+		if (showBottomSheet.value) {
+			ModalBottomSheet(
+				onDismissRequest = { showBottomSheet.value = false },
+				sheetState = sheetState,
+			) { Text("Place Content Here") }
+		}
+		IconButton(
+			onClick = { showBottomSheet.value = !showBottomSheet.value },
+			modifier = Modifier
+				.align(Alignment.BottomStart)
+				.padding(20.dp)
+				.background(
+					color = Color.White,
+					shape = RoundedCornerShape(10.dp)
+				)
+		) {
+				Icon(
+					imageVector = Icons.Default.KeyboardArrowUp,
+					contentDescription = "Menu",
+					modifier = Modifier.size(iconSize)
+				)
+		}
 	}
 }
